33d8a12399e510428655d8fc162a200964137546
danilopezyejas <danilopezyejas@gmail.com>
2025-11-09 08:15:12 -0300

    Apunta Django a DB real y expone modelos en admin

--- amandaye_backend/apps/usuarios/admin.py
+++ amandaye_backend/apps/usuarios/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
-from .models import Socios, Personas
+from .models import (
+    Embarcaciones,
+    Historico_socios,
+    Pagos,
+    Personas,
+    Precios,
+    Recibos,
+    Socios,
+    Socios_cambios,
+)
 
-admin.site.register(Personas)
-admin.site.register(Socios)
+@admin.register(Personas)
+class PersonasAdmin(admin.ModelAdmin):
+    list_display = ("numeroSocio", "nombre_completo", "Cedula")
+    search_fields = ("Cedula", "PrimerNombre", "PrimerApellido", "SegundoApellido")
+
+
+@admin.register(Socios)
+class SociosAdmin(admin.ModelAdmin):
+    list_display = ("numero", "cedulaTitular", "tipo", "fechaAlta", "activo")
+    search_fields = ("numero", "cedulaTitular")
+    list_filter = ("activo", "tipo")
+
+
+admin.site.register(Embarcaciones)
+admin.site.register(Historico_socios)
+admin.site.register(Pagos)
+admin.site.register(Precios)
+admin.site.register(Recibos)
+admin.site.register(Socios_cambios)
